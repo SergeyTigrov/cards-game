b441737705ae1f19d390894eed386b4eec6e262b
SergeyTigrov <sereja.tigrow@yandex.ru>
2025-02-08 23:28:05 +0300

    изменил отображение флагов

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -11,7 +11,7 @@ export default function Card({card, viborCards, visible, disabled}) {
     return(
         <div className="card" id={card.id}>
             <div className={visible ? 'visible' : ''} >
-                <div className='card_img_front'><img src={card.info} alt='country'/></div>
+                <div className='card_img_front'><img src={process.env.PUBLIC_URL + card.info} alt='country'/></div>
                 <div className='card_img_back'><img onClick={() => handleClick()} src='img/earth.png' alt='country'/></div>   
             </div>
         </div>
